09eb36773a72502bf0f6b6bcfd7145b1499235a1
Pauxy <chunyu1399@outlook.com>
2021-05-12 03:36:36 +0800

    added country object and implemented objects into setting and retrieveing of data

--- covid.py
+++ covid.py
@@ -2,10 +2,11 @@
 import requests
 from datetime import date
 import json
+import data
 
 WHOSITE = "https://opendata.arcgis.com/datasets/1cb306b5331945548745a5ccd290188e_2.geojson"
 FILEDAY = None
-COVIDDATA = None
+COVIDDATA = []
 
 
 def gettoday():
@@ -16,17 +17,30 @@ def gettoday():
 def retrieve():
     global FILEDAY
     global COVIDDATA
+    COVIDDATA=[]
     req = requests.get(WHOSITE, allow_redirects=True)
     FILEDAY = gettoday()
-    rawcovidjson = json.loads(req.content)
-    COVIDDATA = rawcovidjson["features"]
+    rawcovidjson = json.loads(req.content)["features"]
+    for country in rawcovidjson:
+        properties = country["properties"]
+        name = properties["Country_Region"]
+        update = properties["Last_Update"]
+        latlong = (properties["Lat"],properties["Long_"])
+        confirmed = properties["Confirmed"]
+        death = properties["Deaths"]
+        recovered = properties["Recovered"]
+        active = properties["Active"]
+        incident = properties["Incident_Rate"]
+        mortality = properties["Mortality_Rate"]
+        currcountry = data.Country(name,update,latlong,confirmed,death,recovered,active,incident,mortality)
+        COVIDDATA.append(currcountry)
+        print(currcountry.print())
+
 
 
 def getcountry(country):
     if FILEDAY == None or gettoday() != FILEDAY:
         retrieve()
-    for country in COVIDDATA:
-        print(country["properties"])
 
 
-print(getcountry("SG"))
+getcountry("SG")
---
+++ data.py
@@ -0,0 +1,32 @@
+class Country:
+    name = None
+    update = None
+    latlong = None
+    confirmed = None
+    death = None
+    recovered = None
+    active = None
+    incident = None
+    mortality = None
+
+    def __init__(self, name, update, latlong, confirmed, death, recovered, active, incident, mortality):
+        self.name = name
+        self.update = update
+        self.latlong = latlong
+        self.confirmed = confirmed
+        self.death = death
+        self.recovered = recovered
+        self.active = active
+        self.incident = incident
+        self.mortality = mortality
+
+    def print(self):
+        beautified = "Name : {0}\n\
+Last Updated : {1}\n\
+Lat Long : {2}\n\
+Confirmed cases : {3}\n\
+Deaths : {4}\n\
+Recovered : {5}\n\
+Active Cases : {6}\n\
+Mortality Rate : {7}%\n".format(self.name, self.update, self.latlong, self.confirmed, self.death, self.recovered, self.active, self.mortality)
+        return beautified
